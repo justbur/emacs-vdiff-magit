cc9e2dbd81d7f717381981501472808b7a4c6d79
Justin Burkett <justin@burkett.cc>
2025-03-08 07:14:02 -0500

    Fix version number of vdiff dependency
    Fixes #11

--- vdiff-magit.el
+++ vdiff-magit.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/justbur/emacs-vdiff-magit
 ;; Version: 0.3.3
 ;; Keywords: diff
-;; Package-Requires: ((emacs "24.4") (vdiff "0.3") (magit "2.10.0") (transient "0.1.0"))
+;; Package-Requires: ((emacs "24.4") (vdiff "0.2.4") (magit "2.10.0") (transient "0.1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
